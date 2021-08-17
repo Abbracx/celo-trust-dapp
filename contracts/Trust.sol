ad5c9a8c0a9c04f1a4c585495fc43d373dc28ae1
Abbracx <tankoraphael@gmail.com>
2021-08-17 16:35:20 +0100

    making changes

--- contracts/Trust.sol
+++ contracts/Trust.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.7.0 <0.9.0;
-pragma experimental ABIEncoderV2;
+// pragma experimental ABIEncoderV2;
 
 interface IERC20Token {
   function transfer(address, uint256) external returns (bool);
@@ -22,6 +22,7 @@ contract Trust {
 
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
     address public admin; // Owner of the contract
+    uint public childCount;
 
     //an enumeration to make sure kid is not added twice
     enum Status {NOT_ADDED, IS_ADDED}
@@ -34,68 +35,82 @@ contract Trust {
         uint when
         );
 
-    //Kid struct type to store kid needed attribute
+    //Kid struct type to store kid needed attributes
     struct Kid {
         string name;
+        address payable child;
         uint amount;
         uint timeToMaturity;
         bool paid;
         Status status;
     }
 
-    mapping (address => Kid) public Kids;
+    mapping (uint => Kid) internal Kids;
+    mapping (string => uint) public KidsIndex;
 
-    Kid [] list_of_kids;
 
   constructor() {
-        admin = msg.sender;
+    childCount = 0;
+    admin = msg.sender;
   }
 
-  function addKid (address _kid, string memory _name, uint _timeToMaturity) external payable {
+    function addKid (address payable _kid,
+                    string memory _name,
+                    uint _timeToMaturity)
+                    external payable {
 
         uint timeToMaturity = block.timestamp + _timeToMaturity;
-        bool _paid = false
+        bool _paid = false;
 
         require (msg.sender == admin, "Only owner can add kid.");
-        require (Kids[_kid].status != Status.IS_ADDED, "Kid already exist");
+        require (Kids[childCount].status == Status.NOT_ADDED, "Kid already exist");
 
-        Kids[_kid] = Kid(_name, msg.value, timeToMaturity, _paid, Status.IS_ADDED);
-        list_of_kids.push(_kid);
+        Kids[childCount] = Kid(_name, _kid, msg.value, timeToMaturity, _paid, Status.IS_ADDED);
+        KidsIndex[_name] = childCount;
+
+
         emit Registered(msg.sender, _kid, block.timestamp);
-  }
-
-  function showKidAmount (address _kid) external view returns(uint, string memory) {
-        return (Kids[_kid].amount, Kids[_kid].name);
-  }
+        childCount++;
+    }
 
 
-  function withdrawAmount() external returns(bool)  {
+    function getKid(string memory _name) external view returns(
+        string memory name,
+        address child,
+        uint amount,
+        uint timeToMaturity,
+        bool paid ){
+
+        if(keccak256(bytes(_name)) == keccak256(bytes(Kids[KidsIndex[_name]].name))){
+            return (
+                Kids[KidsIndex[_name]].name,
+                Kids[KidsIndex[_name]].child,
+                Kids[KidsIndex[_name]].amount,
+                Kids[KidsIndex[_name]].timeToMaturity,
+                Kids[KidsIndex[_name]].paid
+                );
+        }
+
+    }
 
 
-        uint amount;
-        address _kid = msg.sender;
-        Kid memory kid = Kids[_kid];
+    function withdrawAmount(string memory _name) external {
 
+        address _child = msg.sender;
 
-
-        require (kid.amount > 0, "Cannot withdraw zero value.");
-        require (kid.paid == false, "Paid Already.");
-        require (kid.timeToMaturity <= block.timestamp, "Sorry You cannot withdraw.");
+        //only the kid can withdraw his money deposited
+        require (Kids[KidsIndex[_name]].child == _child, "Only valid kid can withdraw");
+        require (Kids[KidsIndex[_name]].amount > 0, "Cannot withdraw zero value.");
+        require (Kids[KidsIndex[_name]].paid == false, "Paid Already.");
+        require (Kids[KidsIndex[_name]].timeToMaturity < block.timestamp, "Sorry You cannot withdraw now.");
 
         // payable(_kid).transfer(kid.amount);
         // code below is to prevent reeentrancy as oppose to msg.sender.transfer(Amount)
-        amount = kid.amount;
-        kid.amount = 0;
-        (bool success, ) = payable(_kid).call{value:amount}("");
+
+        (bool success, ) = payable(_child).call{value:Kids[KidsIndex[_name]].amount}("");
         require(success, "Transfer failed.");
-        kid.paid = true;
-        return success;
+        Kids[KidsIndex[_name]].paid = true;
   }
-
-  function getAllkids () returns(Kid[] memory) external {
-      return list_of_kids
-  }
-
 
 
 }
